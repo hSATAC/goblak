6974784ac49fbb7a9688fa732f7a6c0fc747bcca
hSATAC <hsatac@gmail.com>
2014-03-25 16:36:11 +0800

    Init

---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	_ "image/jpeg" // Register JPEG format
+	"image/png"    // Register PNG  format
+	"log"
+	"net/http"
+)
+
+// Converted implements image.Image, so you can
+// pretend that it is the converted image.
+type Converted struct {
+	Img image.Image
+	Mod color.Model
+}
+
+// We return the new color model...
+func (c *Converted) ColorModel() color.Model {
+	return c.Mod
+}
+
+// ... but the original bounds
+func (c *Converted) Bounds() image.Rectangle {
+	return c.Img.Bounds()
+}
+
+// At forwards the call to the original image and
+// then asks the color model to convert it.
+func (c *Converted) At(x, y int) color.Color {
+	return c.Mod.Convert(c.Img.At(x, y))
+}
+
+func main() {
+	http.Handle("/gray", http.HandlerFunc(convertGray))
+	http.Handle("/bw", http.HandlerFunc(convertBw))
+	err := http.ListenAndServe(":2003", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
+
+func convertGray(w http.ResponseWriter, req *http.Request) {
+	img := imageFromURL(req.FormValue("url"))
+
+	gr := &Converted{img, color.GrayModel}
+
+	w.Header().Set("Content-Type", "image/png")
+	png.Encode(w, gr)
+}
+
+func convertBw(w http.ResponseWriter, req *http.Request) {
+	img := imageFromURL(req.FormValue("url"))
+
+	bw := []color.Color{color.Black, color.White}
+	gr := &Converted{img, color.Palette(bw)}
+
+	w.Header().Set("Content-Type", "image/png")
+	png.Encode(w, gr)
+}
+
+func imageFromURL(imgUrl string) image.Image {
+	if imgUrl == "" {
+		return nil // Handle empty parameter here
+	}
+
+	res, err := http.Get(imgUrl)
+	if err != nil || res.StatusCode != 200 {
+		log.Fatalln(err)
+	}
+
+	img, _, err := image.Decode(res.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return img
+}
